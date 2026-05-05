855ff2b369e466f3406918d167690b5268aa04cc
Uday vara <varaudayd@gmail.com>
2026-05-05 12:45:34 +0530

    added cors

--- src/main.py
+++ src/main.py
@@ -3,12 +3,21 @@ from fastapi import FastAPI
 from src.utils.db.session import Engine
 from src.models.models import *
 from src.User.router import router as UserRouter
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 # Bind Base & Engine
 Base.metadata.create_all(Engine)
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # change this in production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
